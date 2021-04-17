84df9350282dfe11202be8548329a880b5042aac
IvanSampere <IvanSampere@DESKTOP-F53DEEE>
2021-04-17 09:02:42 +0200

    styles, login html and register html

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, request
+
 
 app = Flask("__name__")
 
@@ -6,6 +7,13 @@ app = Flask("__name__")
 def index():
     return render_template("index.html")
 
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        return redirect("/")
+    return render_template("login.html")
+
 
 if __name__ == "__main__":
     app.run()
+
