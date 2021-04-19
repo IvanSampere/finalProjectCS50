132043e0879357592eb366ed63e8798a7d58f2be
IvanSampere <IvanSampere@DESKTOP-F53DEEE>
2021-04-19 20:51:25 +0200

    js login and register, logic login and register, start logic content and start html content

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, session, redirect, request
+from flask import Flask, render_template, session, redirect, request, flash
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -35,16 +35,21 @@ def login():
 
     if request.method == "POST":
         # Get the data from the form
-        username = request.form.get("username")
+        username = request.form.get("username") # Also for the email
         password = request.form.get("password")
         # Query database for username
-        row_user = db.execute("SELECT * FROM users WHERE username = ?", username)
+        row_user = db.execute("SELECT * FROM users WHERE username = ? OR email = ?", username, username)
         # Check the username exist and the password is correct
-        if len(row_user) == 1 and check_password_hash(row_user[0]['hash'], password):
-            session['user_id'] = row_user[0]['id']
-        # redirect to the index
+        if len(row_user) != 1 or not check_password_hash(row_user[0]['hash'], password):
+            flash("Invalid user/email or password")
+            return redirect("/login")
+
+        # Keep the data from the session
+        session['user_id'] = row_user[0]['id']
+        session['username'] = row_user[0]['username']
+        # redirect to the index    
         return redirect("/")
-    
+
     return render_template("login.html")
 
 
@@ -57,6 +62,15 @@ def register():
         password = request.form.get("password")
         # Generate a password hash
         password = generate_password_hash(password)
+        # Ensure the username is not taken
+        usernames = db.execute("SELECT * FROM users")
+        for username_list in usernames:
+            if username == username_list['username']:
+                flash("The username already taken")
+                return redirect("/register")
+            if email == username_list['email']:
+                flash("The email already exist, try to log in")
+                return redirect("/register")
         # Execute a query to insert the data to the table users
         db.execute("INSERT INTO users (username, email, hash) VALUES (?, ?, ?)", 
                     username, email, password)
@@ -74,6 +88,15 @@ def log_out():
     # redirect to the index
     return redirect("/")
 
+@app.route("/content", methods=['GET', 'POST'])
+def content():
+    if request.method == 'POST':
+        tag = request.form.getlist("tag")
+        print(tag[0])
+        return redirect("/content")
+    else:
+        content = request.args.get("q")
+        return render_template("content.html")
 
 @app.route("/new_activity", methods=["GET", "POST"])
 def new_activity():
