6a5f7291bc3d8086007331b6d069cd67fe1889dc
IvanSampere <ivansampere@hotmail.com>
2021-04-19 13:29:51 +0200

    connection database, login and register

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import os
+
 from flask import Flask, render_template, session, redirect, request
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
-import sqlite3
+from cs50 import SQL
 
 
 
@@ -18,9 +20,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Connect with the db
-db = sqlite3.connect("project.db")
-cursor = db.cursor()
-
+db = SQL('sqlite:///project.db')
 
 @app.route("/")
 def index():
@@ -30,35 +30,49 @@ def index():
 
 @app.route("/login", methods=["GET","POST"])
 def login():
-    """ TODO """
+    # Forget any user id
+    session.clear()
+
     if request.method == "POST":
+        # Get the data from the form
+        username = request.form.get("username")
+        password = request.form.get("password")
+        # Query database for username
+        row_user = db.execute("SELECT * FROM users WHERE username = ?", username)
+        # Check the username exist and the password is correct
+        if len(row_user) == 1 and check_password_hash(row_user[0]['hash'], password):
+            session['user_id'] = row_user[0]['id']
+        # redirect to the index
         return redirect("/")
+    
     return render_template("login.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    
     if request.method == "POST":
+        # Get the data from the form
         username = request.form.get("username")
         email = request.form.get("email")
         password = request.form.get("password")
-
+        # Generate a password hash
         password = generate_password_hash(password)
-        
-        cursor.executemany("INSERT INTO users (username, email, hash) VALUES (?, ?, ?)", 
-                        username, email, password)
-        db.commit()
-
-        return redirect("/register")
-
+        # Execute a query to insert the data to the table users
+        db.execute("INSERT INTO users (username, email, hash) VALUES (?, ?, ?)", 
+                    username, email, password)
+        # Login directly
+        login()
+        # redirect to the index
+        return redirect("/")
     return render_template("register.html")
 
 
 @app.route("/log_out")
 def log_out():
-    """ TODO """
-    return ("log out")
+    # forget any user_id
+    session.clear()
+    # redirect to the index
+    return redirect("/")
 
 
 @app.route("/new_activity", methods=["GET", "POST"])
